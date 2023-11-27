53e0d8bfe2344b28d941dd9a3fd47a62272711b3
Tinfou <144536026+Tinfou@users.noreply.github.com>
2023-11-27 10:05:36 +0100

    creation de classe

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-<?php echo "first commit poo"; ?>
+<?php
+class Personne
+{
+  public string $nom;
+  private int $age;
+  protected string $addresse;
+  protected int $retraite = 60;
+
+   public function getNom(): string{
+        return $this->nom;
+    }
+
+    public function setNom(string $newNom){
+      $this->nom = $newNom;
+      return $this;
+    }
+    public function getAge(): int{
+        return $this->age;
+    }
+    public function setAge(int $newAge){
+      $this->age = $newAge;
+      return $this;
+    }
+    public function getAnneeServiceRestante(int $age){
+      return $this->retraite - $this->age;
+    }
+}
+
+$pers = new Personne();
+$age = $pers->setAge(20);
+$serviceRestante = $pers->getAnneeServiceRestante($pers->getAge());
+echo $serviceRestante;
+
+
+
+?>
