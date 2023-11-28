23c24274d90d68ab99110b94c43f551d075ef068
Tinfou <144536026+Tinfou@users.noreply.github.com>
2023-11-28 08:42:52 +0100

    exercice sur les methodes

--- index.php
+++ index.php
@@ -4,7 +4,14 @@ class Personne
   public string $nom;
   private int $age;
   protected string $addresse;
-  protected int $retraite = 60;
+  private int $retraite = 60;
+
+  public function __construct(string $nom, int $age, string $addresse, int $retraite){
+    $this->nom = $nom;
+    $this->age = $age; 
+    $this->addresse = $addresse;  
+    $this->retraite = $retraite; 
+  }
 
    public function getNom(): string{
         return $this->nom;
@@ -18,16 +25,26 @@ class Personne
         return $this->age;
     }
     public function setAge(int $newAge){
+      self :: verificationAge($newAge);
       $this->age = $newAge;
       return $this;
     }
     public function getAnneeServiceRestante(int $age){
       return $this->retraite - $this->age;
     }
+    public function verificationAge($age):bool{
+      if ($age>18){
+        return true;
+      }
+      else{
+        echo "age diso";
+        return false;
+        
+      }
+    }
 }
 
-$pers = new Personne();
-$age = $pers->setAge(20);
+$pers = new Personne('mahery',25,'test',50);
 $serviceRestante = $pers->getAnneeServiceRestante($pers->getAge());
 echo $serviceRestante;
 
